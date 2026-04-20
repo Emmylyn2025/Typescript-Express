7304cd3023995ea61e95e8766bdc5097fd718742
Awobodu Emmanuel <awoboduemmanuel@gmail.com>
2026-04-20 20:24:11 +0100

    Trying to debug .env 2

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import app from "./app";
 
 const PORT = Number(process.env.PORT) || 3000;
 
+console.log(process.env.PORT);
+
 app.listen(PORT ,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
